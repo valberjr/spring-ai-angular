37cb77e2cf3ac27bd7c56db8b630ca6681bb9e3f
valberjr <valber@duck.com>
2025-05-10 09:46:49 -0300

    first commit

---
+++ api-ai/src/main/java/com/example/api_ai/ApiAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAiApplication.class, args);
+	}
+
+}
