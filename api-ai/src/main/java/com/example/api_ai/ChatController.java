003e8a6a02a3dbbb3473232ade2314e725f4ea19
valberjr <valber@duck.com>
2025-05-10 12:44:27 -0300

    feat: openai

---
+++ api-ai/src/main/java/com/example/api_ai/ChatController.java
@@ -0,0 +1,23 @@
+package com.example.api_ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
