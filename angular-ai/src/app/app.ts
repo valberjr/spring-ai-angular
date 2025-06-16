009fdc87b8075b03d7fe5f66dbe3de7f43579040
valberjr <valber@duck.com>
2025-06-16 17:26:56 -0300

    feat: create angular project

---
+++ angular-ai/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  readonly title = 'AI Spring-Angular';
+}
